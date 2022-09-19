9beada4de2669a8eadad4a164c50f6c84ac86134
Jake Cyr <cyrjake@gmail.com>
2022-09-18 22:20:34 -0400

    Init

---
+++ README.md
@@ -0,0 +1,19 @@
+# Next Export Lang
+
+A simple npm package to add the HTML lang attribute to all export files after building to satisfy the Lighthouse accessibility rule for SEO.
+
+## Installation and Usage
+
+### Global CLI Usage
+
+1. Run `npm install -g next-export-lang`
+2. Example: run `next-export-lang en-US` when in a Next.JS project after running the `next export` command to add HTML 'lang' attributes to all HTML files in the 'out' folder.
+
+### Local Project Installation
+
+The next-export-lang package can be installed in a Node project so it can be integrated as part of your build process.
+
+1. Run `npm install -D next-export-lang`
+2. Add `next-export-lang` to your build command (after `next export`). For example, in your package.json, update your build script, adding `&& next-export-lang en` to the end, replacing 'en' with the language you want to add to your HTML files: `"build": "next export && next-export-lang"`.
+
+A lang attribute with the defined language will be added to all HTML files in the 'out' folder generated from the `next export` command.
---
+++ index.js
@@ -0,0 +1,61 @@
+#! /usr/bin/env node
+
+const cheerio = require('cheerio');
+const fs = require('fs/promises');
+const path = require('path');
+const glob = require('glob');
+const minify = require('html-minifier').minify;
+
+const args = process.argv.slice(2);
+const lang = args.length > 0 ? args[0] : 'en';
+
+(async () => {
+  const outFolder = path.join(__dirname, './out');
+  const outFolderExists = await pathExists(outFolder);
+
+  if (!outFolderExists) {
+    console.error(`'out' folder does not exist yet. Run 'next export' first.`);
+    process.exit(1);
+  }
+
+  const htmlFiles = await findFiles(outFolder + '/**/*.html');
+  console.log(`Found ${htmlFiles.length} HTML files in the 'out' folder.`);
+  console.log(`Adding lang="${lang}" to all files...`);
+
+  await Promise.all(
+    htmlFiles.map(async (filePath) => {
+      const rawHTML = (await fs.readFile(filePath)).toString();
+      const $ = cheerio.load(rawHTML);
+
+      $('html').attr('lang', lang);
+
+      const outputHTML = $.html();
+      const minifiedHTML = minify(outputHTML);
+
+      console.log(`Adding 'lang' attribute to html element in file: ${filePath}...`);
+      await fs.writeFile(filePath, minifiedHTML);
+    }),
+  );
+
+  console.log('Added lang attributes to all files.');
+})();
+
+async function pathExists(pathToCheck) {
+  try {
+    await fs.stat(pathToCheck);
+    return true;
+  } catch {
+    return false;
+  }
+}
+
+function findFiles(searchPattern) {
+  return new Promise((resolve, reject) => {
+    glob(searchPattern, (err, matches) => {
+      if (err) {
+        return reject(err);
+      }
+      resolve(matches);
+    });
+  });
+}
